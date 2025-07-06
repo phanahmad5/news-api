0a4349ab4f4c26b3e257eaae83ce3d380fec6b78
phanahmad5 <phanahmad63@gmail.com>
2025-07-06 18:16:06 +0700

    add news api routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,10 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NewsController;
 
-Route::apiResource('news', NewsController::class);
+Route::get('/news', [NewsController::class, 'index']);
+Route::post('/news', [NewsController::class, 'store']);
+Route::put('/news/{id}', [NewsController::class, 'update']);
+Route::delete('/news/{id}', [NewsController::class, 'destroy']);
